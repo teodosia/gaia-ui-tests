e25ba7b424bf6590cf12ca7b155b2dc1fe434969
Teodosia Pop <teodosia.pop@gmail.com>
2013-08-07 17:36:39 +0300

    Migrate test_everythingme_launch_app.py to use the homescreen search bar

--- gaiatest/tests/everythingme/test_everythingme_launch_app.py
+++ gaiatest/tests/everythingme/test_everythingme_launch_app.py
@@ -2,29 +2,51 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from marionette.by import By
+from marionette.keys import Keys
 from gaiatest import GaiaTestCase
-from gaiatest.apps.everythingme.app import EverythingMe
 
 
 class TestEverythingMeLaunchApp(GaiaTestCase):
+
+    _rocket_bar_icon_locator = (By.ID, 'rocketbar-activation-icon')
+    _rocket_search_box_locator = (By.ID, 'rocketbar-input')
+    _search_results_locator = (By.CSS_SELECTOR, '#rocketbar-search-results .visual')
+    _search_results_from_everything_me = (By.CSS_SELECTOR, '#rocketbar-search-results .visual > small')
 
     def setUp(self):
         GaiaTestCase.setUp(self)
         self.apps.set_permission('Homescreen', 'geolocation', 'deny')
         self.connect_to_network()
-        self.everythingme = EverythingMe(self.marionette)
 
     def test_launch_everything_me_app(self):
         # https://github.com/mozilla/gaia-ui-tests/issues/69
 
         app_name = 'Twitter'
-        self.everythingme.go_to_everything_me()
+        self.homescreen = self.apps.launch('Homescreen')
+        self._tap_rocket_bar()
+        self._type_into_rocket_search_box(app_name)
 
-        self.everythingme.wait_for_categories_to_load()
-        self.assertGreater(self.everythingme.categories_count, 0, 'No shortcut categories found')
-        self.everythingme.tap_category('social')
+        self._wait_for_everthimg_me_results_to_load()
 
-        self.everythingme.wait_for_app_icons_displayed()
-        self.everythingme.tap_app_icon(app_name)
+        # Tap on firt icon returned from Everthing.Me category
+        self._tap_first_result()
 
         self.assertIn(app_name, self.marionette.title)
+
+    def _type_into_rocket_search_box(self, search_term):
+        search_input = self.marionette.find_element(*self._rocket_search_box_locator)
+        search_input.clear()
+        search_input.send_keys(search_term)
+        search_input.send_keys(Keys.RETURN)
+
+    def _tap_rocket_bar(self):
+        rocket_bar = self.marionette.find_element(*self._rocket_bar_icon_locator)
+        rocket_bar.tap()
+
+    def _wait_for_everthimg_me_results_to_load(self):
+        self.wait_for_element_displayed(*self._search_results_locator)
+
+    def _tap_first_result(self):
+        result = self.marionette.find_element(*self._search_results_from_everything_me)
+        result.tap()
